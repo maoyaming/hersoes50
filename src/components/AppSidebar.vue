f5011aaa5ed01dcadbe86aaf33ead4e6305a0ec3
MaoYaming <249945984@qq.com>
2018-09-02 12:08:03 +0800

    分类插件

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,17 +1,29 @@
 <template>
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-    <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+    <!-- <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
     <li><a href="#">Reports</a></li>
     <li><a href="#">Analytics</a></li>
-    <li><a href="#">Export</a></li>
+    <li><a href="#">Export</a></li> -->
+
+
+
+    <router-link tag="li" to="/heroes">
+        <a> 英雄管理</a>
+    </router-link>
+    <router-link tag="li" to="/weapons">
+        <a> 装备管理</a>
+    </router-link>
+    <router-link tag="li" to="/equips">
+        <a> 人物管理</a>
+    </router-link>
     </ul>
 </div>
 </template>
 <script>
-    export default {
+    // export default {
 
-    }
+    // }
 </script>
 <style >
     
--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 //  程序入口
 
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 // 导入全局样式
 // 导入bootstrap样式
-import 'bootstrap/dist/css/bootstrap.css'
-import './assets/css/index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.css';
 
+
+// 导入路由组件
+import HeroList from './views/heroes/List.vue';
+import WeqList from './views/weqpons/List.vue';
+import EqList from './views/equips/List.vue';
+
+
+// Vue.use()注册VueRouter插件
+Vue.use(VueRouter);
+const router = new VueRouter({
+    // 设置精确的激活类样式
+    linkExactActiveClass: 'active',
+    //routes 路由规则  --->配置路由规则要有组件
+    routes: [{
+        path: '/heroes',
+        component: HeroList
+    }, {
+        path: '/weapons',
+        component: WeqList
+    }, {
+        path: '/equips',
+        component: EqList
+    }]
+})
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 })
