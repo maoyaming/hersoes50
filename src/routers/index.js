78ac8915f6ec20064e910a99f1a5ff2fccea3959
MaoYaming <249945984@qq.com>
2018-09-02 15:35:18 +0800

    人物删除

---
+++ src/routers/index.js
@@ -0,0 +1,38 @@
+//  将路由分离出来  
+
+
+
+
+// 导入路由组件
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+
+import HeroList from '../views/heroes/List.vue';
+import WeqList from '../views/weqpons/List.vue';
+import EqList from '../views/equips/List.vue';
+
+
+// Vue.use()注册VueRouter插件
+Vue.use(VueRouter);
+const router = new VueRouter({
+    // 设置精确的激活类样式
+    linkExactActiveClass: 'active',
+    //routes 路由规则  --->配置路由规则要有组件
+    routes: [{
+        path: '/',
+        redirect: '/heroes'
+    }, {
+        path: '/heroes',
+        component: HeroList
+    }, {
+        path: '/weapons',
+        component: WeqList
+    }, {
+        path: '/equips',
+        component: EqList
+    }]
+});
+
+//导出对象
+export default router;
